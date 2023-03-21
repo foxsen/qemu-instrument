5906a69d575d36de4e2807f1faeaf068d731c42f
Liu Minjie <liuminjie@loongson.cn>
2023-03-21 12:00:15 +0800

    fix: sign_extend use unsigned long

--- target/loongarch/instrument/util/bitopts.h
+++ target/loongarch/instrument/util/bitopts.h
@@ -1,24 +1,27 @@
 #ifndef _LA_BITOPTS_H_
 #define _LA_BITOPTS_H_
 
+#include <stdint.h>
+
 /* n: 0~63 */
 #define BitSet(x, n) (x | (1 << n))
 #define BitUnset(x, n) (x & ~(1 << n))
 #define BitIsSet(x, n) (x & (1 << n))
 #define ToggleBit(x, n) (x ^ (1 << n))
 
-#include <stdint.h>
-#include "error.h"
 /* 符号扩展到64位，wide表示原始数据位宽（1~63） */
 static inline uint64_t sign_extend(uint64_t val, int wide)
 {
-    /* FIXME: how to deal with wide == 64? */
-    lsassert(1 <= wide && wide < 64);
-    /* if sign bit of addr is 1, extend */
-    uint64_t sign = val & (1 << (wide - 1));
+    if (wide == 0) {
+        return 0;
+    }
+    if (wide >= 64) {
+        return val;
+    }
+
+    uint64_t sign = val & (1UL << (wide - 1));
     if (sign) {
-        uint64_t ones = -1UL << wide;
-        val = ones | val;
+        val = val | (-1UL << wide);
     } else {
         val = val & ((1UL << wide) - 1);
     }
