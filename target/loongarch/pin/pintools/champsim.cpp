a603df8c183c22143be118633521c5a42387e19c
xuefeng <xuefeng@loongson.cn>
2023-04-27 16:37:58 +0800

    pin_tool champsim, use op
    Signed-off-by: xuefeng <xuefeng@loongson.cn>

--- target/loongarch/pin/pintools/champsim.cpp
+++ target/loongarch/pin/pintools/champsim.cpp
@@ -18,7 +18,8 @@ typedef struct trace_instr_format {
     unsigned long long int destination_memory[NUM_INSTR_DESTINATIONS]; // output memory
     unsigned long long int source_memory[NUM_INSTR_SOURCES];           // input memory
     unsigned long long ret_val;
-    unsigned int insn;
+    //unsigned int insn;
+    unsigned short op;
     unsigned char is_branch;    // is this branch
     unsigned char branch_taken; // if so, is this taken
 
@@ -64,7 +65,7 @@ static VOID ins_load_before(UINT64 pc, UINT64 addr)
     ins_load_addr = addr;
 }
 
-static VOID ins_load_after(UINT64 pc, UINT64 reg_rw, UINT64 ret_val, UINT32 insn)
+static VOID ins_load_after(UINT64 pc, UINT64 reg_rw, UINT64 ret_val, /*UINT32 insn,*/ UINT16 op)
 {
     if (icount < icount_begin) {
         return;
@@ -77,14 +78,15 @@ static VOID ins_load_after(UINT64 pc, UINT64 reg_rw, UINT64 ret_val, UINT32 insn
         .ip = pc,
         .source_memory = {ins_load_addr},
         .ret_val = ret_val,
-        .insn = insn,
+        //.insn = insn,
+        .op = op,
         .destination_registers = {reg_rw_w(reg_rw)},
         .source_registers = {reg_rw_r0(reg_rw), reg_rw_r1(reg_rw), reg_rw_r2(reg_rw)},
     };
     put_trace(&trace);
 }
 
-static VOID ins_store(UINT64 pc, UINT64 addr, UINT64 reg_rw, UINT32 insn)
+static VOID ins_store(UINT64 pc, UINT64 addr, UINT64 reg_rw, /*UINT32 insn,*/ UINT16 op)
 {
     if (icount < icount_begin) {
         return;
@@ -96,14 +98,15 @@ static VOID ins_store(UINT64 pc, UINT64 addr, UINT64 reg_rw, UINT32 insn)
     trace_instr_format_t trace = {
         .ip = pc,
         .destination_memory = {addr},
-        .insn = insn,
+        //.insn = insn,
+        .op = op,
         .destination_registers = {reg_rw_w(reg_rw)},
         .source_registers = {reg_rw_r0(reg_rw), reg_rw_r1(reg_rw), reg_rw_r2(reg_rw)},
     };
     put_trace(&trace);
 }
 
-static VOID ins_br(UINT64 pc, UINT64 taken, UINT64 reg_rw, UINT64 ret_val, UINT32 insn)
+static VOID ins_br(UINT64 pc, UINT64 taken, UINT64 reg_rw, UINT64 ret_val, /*UINT32 insn,*/ UINT16 op)
 {
     if (icount < icount_begin) {
         return;
@@ -115,7 +118,8 @@ static VOID ins_br(UINT64 pc, UINT64 taken, UINT64 reg_rw, UINT64 ret_val, UINT3
     trace_instr_format_t trace = {
         .ip = pc,
         .ret_val = pc + 4,
-        .insn = insn,
+        //.insn = insn,
+        .op = op,
         .is_branch = 1,
         .branch_taken = (unsigned char)taken,
         .destination_registers = {reg_rw_w(reg_rw)},
@@ -124,7 +128,7 @@ static VOID ins_br(UINT64 pc, UINT64 taken, UINT64 reg_rw, UINT64 ret_val, UINT3
     put_trace(&trace);
 }
 
-static VOID ins_others(UINT64 pc, UINT64 reg_rw, UINT64 ret_val, UINT32 insn)
+static VOID ins_others(UINT64 pc, UINT64 reg_rw, UINT64 ret_val, /*UINT32 insn,*/ UINT16 op)
 {
     if (icount < icount_begin) {
         return;
@@ -136,7 +140,8 @@ static VOID ins_others(UINT64 pc, UINT64 reg_rw, UINT64 ret_val, UINT32 insn)
     trace_instr_format_t trace = {
         .ip = pc,
         .ret_val = ret_val,
-        .insn = insn,
+        //.insn = insn,
+        .op = op,
         .destination_registers = {reg_rw_w(reg_rw)},
         .source_registers = {reg_rw_r0(reg_rw), reg_rw_r1(reg_rw), reg_rw_r2(reg_rw)},
     };
@@ -180,17 +185,17 @@ static VOID Instruction(INS ins, VOID* v)
         gpr_w = 0;
     }
     gpr_w += REG_GR_BASE;
-    unsigned int insn = ins->opcode;
-    // unsigned int insn = ins->origin_ins->op;
+    //unsigned int insn = ins->opcode;
+    unsigned short op = ins->origin_ins->op;
     if (INS_IsMemoryRead(ins)) {
         INS_InsertCall(ins, IPOINT_BEFORE, (AFUNPTR)ins_load_before, IARG_UINT64, ins->pc, IARG_MEMORYOP_EA, 0, IARG_END);
-        INS_InsertCall(ins, IPOINT_AFTER, (AFUNPTR)ins_load_after, IARG_UINT64, ins->pc, IARG_UINT64, rw, IARG_REG_VALUE, gpr_w, IARG_UINT64, insn, IARG_END);
+        INS_InsertCall(ins, IPOINT_AFTER, (AFUNPTR)ins_load_after, IARG_UINT64, ins->pc, IARG_UINT64, rw, IARG_REG_VALUE, gpr_w, IARG_UINT64, /*insn,*/ op, IARG_END);
     } else if (INS_IsMemoryWrite(ins)) {
-        INS_InsertCall(ins, IPOINT_BEFORE, (AFUNPTR)ins_store, IARG_UINT64, ins->pc, IARG_MEMORYOP_EA, 0, IARG_UINT64, rw, IARG_UINT64, insn, IARG_END);
+        INS_InsertCall(ins, IPOINT_BEFORE, (AFUNPTR)ins_store, IARG_UINT64, ins->pc, IARG_MEMORYOP_EA, 0, IARG_UINT64, rw, IARG_UINT64, /*insn,*/ op, IARG_END);
     } else if (INS_IsBranch(ins)) {
-        INS_InsertCall(ins, IPOINT_BEFORE, (AFUNPTR)ins_br, IARG_UINT64, ins->pc, IARG_BRANCH_TAKEN, IARG_UINT64, rw, IARG_REG_VALUE, gpr_w, IARG_UINT64, insn, IARG_END);
+        INS_InsertCall(ins, IPOINT_BEFORE, (AFUNPTR)ins_br, IARG_UINT64, ins->pc, IARG_BRANCH_TAKEN, IARG_UINT64, rw, IARG_REG_VALUE, gpr_w, IARG_UINT64, /*insn,*/ op, IARG_END);
     } else {
-        INS_InsertCall(ins, IPOINT_AFTER, (AFUNPTR)ins_others, IARG_UINT64, ins->pc, IARG_UINT64, rw, IARG_REG_VALUE, gpr_w, IARG_UINT64, insn, IARG_END);
+        INS_InsertCall(ins, IPOINT_AFTER, (AFUNPTR)ins_others, IARG_UINT64, ins->pc, IARG_UINT64, rw, IARG_REG_VALUE, gpr_w, IARG_UINT64, /*insn,*/ op, IARG_END);
     }
 }
 
